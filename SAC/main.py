f5e47215982ac3296c0bdf745d94c297f703a6d0
t19319 <t19319@kist.re.kr>
2020-10-18 21:36:44 +0600

    changed main

--- SAC/main.py
+++ SAC/main.py
@@ -3,32 +3,47 @@ from agent import Agent
 import time
 from cartpole import CartPoleEnv, CartPoleEnvState
 import matplotlib.pyplot as plt
+import argparse
 
 
 if __name__ == '__main__':
-    env_id = 'CustomCurtpoleEnv_exp_reward_state'
+    parser = argparse.ArgumentParser(description="Pass tau and theta threshold")
+    parser.add_argument("--tau", type=float, help="Need a floating number")
+    parser.add_argument("--theta_threshold", type=float,
+                        help="Need a floating number")
+    args = parser.parse_args()
+    delta_t = args.tau
+    theta_threshold = args.theta_threshold
+
+    env_id = 'BalancePoll_deltat_' + str(delta_t) + '_theta_threshold_'\
+        + str(theta_threshold)
+
     env = CartPoleEnvState(mode='train')
+    env.tau = delta_t
+    env.theta_threshold_radians = theta_threshold
+    
     agent = Agent(alpha=0.0003, beta=0.0003, reward_scale=2,
                   env_id=env_id, input_dims=env.observation_space.shape,
                   tau=0.005, env=env, batch_size=256, layer_1_size=256,
                   layer_2_size=256, n_actions=env.action_space.shape[0])
 
-    n_games = 800
-    filename = env_id + '_' + str(n_games) + 'games_scale' + '_' + \
-        str(agent.scale) + '.png'
-    figure_file = 'plots/' + filename
-
     best_score = env.reward_range[0]
     score_history = []
     load_checkpoint = False
-    thetas = []
+
 
     if load_checkpoint:
         agent.load_models()
+        angle = []
+        position = []
+        thetas = []
+
+    n_games = 1 if load_checkpoint else 800
+    max_steps = 1000 if load_checkpoint else 2000 * int(0.01 // delta_t)
     
-    angle = []
-    
-    max_steps = 500 if load_checkpoint else 2000
+    filename = env_id + '_' + str(n_games) + 'games_scale' + '_' + \
+        str(agent.scale) + '.png'
+    figure_file = 'plots/' + filename
 
     for i in range(n_games):  
         steps = 0
@@ -36,10 +51,13 @@ if __name__ == '__main__':
         done = False
         observation = env.reset()
         while not done:
-            angle.append(observation[2])
+            if load_checkpoint:
+                angle.append(env.theta_desired - observation[2])
+                position.append(env.pos_desired - observation[0])
             action = agent.choose_action(observation)
             observation_, reward, done, info = env.step(action)
             steps += 1
+
             if steps == max_steps:
                 done = True
                 reward = 0
@@ -51,7 +69,7 @@ if __name__ == '__main__':
                 agent.learn()
             else:
                 env.render()
-                time.sleep(0.001)
+                time.sleep(0.01)
 
             observation = observation_
             
@@ -71,12 +89,20 @@ if __name__ == '__main__':
               'scale', agent.scale)
 
     if load_checkpoint:
-        plt.plot(angle, label='pole angle')
-        plt.plot(np.zeros((len(angle), 1)), 'r--', label='0 radian')
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10)) 
+        ax1.plot(angle, label='pole angle')
+        ax1.plot(np.zeros((len(angle), 1)), 'r--', label='0 radian')
 
-        plt.xlabel('episodes')
-        plt.ylabel('pole angle')
-        plt.legend()
-        plt.savefig("pole.png")
+        ax1.set_xlabel('episodes')
+        ax1.set_ylabel('pole angle')
+        ax1.legend()
+        
+        ax2.plot(position, label='cart position')
+        ax2.plot(np.zeros((len(position), 1)) + env.pos_desired, 'r--',
+                 label='reference position')
 
+        ax2.set_xlabel('episodes')
+        ax2.set_ylabel('cart position')
+        ax2.legend()
+        plt.savefig('pos_angle.png')
     env.close()
